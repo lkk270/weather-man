5452f869ed69c515ad9dc68b9344cacea8f62eac
Lee Korff-Korn <Leekk270@gmail.com>
2024-12-08 10:27:22 -0500

    cron change

--- infrastructure/main.tf
+++ infrastructure/main.tf
@@ -89,7 +89,7 @@ resource "aws_lambda_permission" "allow_eventbridge" {
 resource "aws_cloudwatch_event_rule" "hourly" {
   name                = "hourly-weather-pipeline-${terraform.workspace}"
   description         = "Triggers weather pipeline at the start of every hour"
-  schedule_expression = "rate(5 minutes)"  # Simpler and more reliable for testing
+  schedule_expression = "cron(*/5 * * * ? *)"  # Run every 5 minutes
 
   tags = {
     Name        = "weather-pipeline-trigger"
